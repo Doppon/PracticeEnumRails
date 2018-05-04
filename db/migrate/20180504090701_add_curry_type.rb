c389ea4a709fe80f00d8f7b879855efddc5b2288
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-04 18:16:12 +0900

    curry_typeカラムを追加

--- db/migrate/20180504090701_add_curry_type.rb
+++ db/migrate/20180504090701_add_curry_type.rb
@@ -1,6 +1,6 @@
 class AddCurryType < ActiveRecord::Migration[5.1]
   def change
-    add_column :table_name, :column_name, :column_type, :column_options
+    add_column :lunch_sets, :curry_type, :integer, :before => :timestamps
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end

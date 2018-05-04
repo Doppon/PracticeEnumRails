e41092ea5f0ca2e05e335ca73c402f78c24aaf09
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-04 18:09:10 +0900

    カラム追加の雛形を追加

--- db/migrate/20180504090701_add_curry_type.rb
+++ db/migrate/20180504090701_add_curry_type.rb
@@ -1,4 +1,6 @@
 class AddCurryType < ActiveRecord::Migration[5.1]
   def change
+    add_column :table_name, :column_name, :column_type, :column_options
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end

ad48b3bc7fa129bfe088a17c5075257a587a667d
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-20 23:52:09 +0900

    オプションの削除

--- db/migrate/20180504090701_add_curry_type.rb
+++ db/migrate/20180504090701_add_curry_type.rb
@@ -1,6 +1,6 @@
 class AddCurryType < ActiveRecord::Migration[5.1]
   def change
-    add_column :lunch_sets, :curry_type, :integer, :before => :timestamps
+    add_column :lunch_sets, :curry_type, :integer
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end
